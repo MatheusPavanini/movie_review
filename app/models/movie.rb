d5181f4e6029a203155f18008c17eb595026e44c
Matheus <matheus.ppj@gmail.com>
2017-02-19 00:52:44 -0300

    Adicionado search gem

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
+    searchkick
 	belongs_to :user
-
   has_attached_file :image,styles: { medium: "400x600#"}, default_url: "/images/:style/missing.png"#Gem paperclip
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
